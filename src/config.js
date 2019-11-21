175daac6cce37fea8216046e3c8d3b78e3475d0a
Daniel <herrnichols@gmail.com>
2019-11-21 13:45:00 -0500

    updated DB url. Deploying DB

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL || 'postgresql://daniel@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'deustchewoerter',
+  JWT_SECRET: process.env.JWT_SECRET || 'deutschewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };

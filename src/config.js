ec1dab19ea565cae0f963fe0491a6cffde967935
Daniel <herrnichols@gmail.com>
2019-11-21 12:58:23 -0500

    prepping to deploy DB

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DB_URL || 'postgresql://daniel@localhost/spaced-repetition',
+    process.env.DATABASE_URL || 'postgresql://daniel@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'deustchewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };

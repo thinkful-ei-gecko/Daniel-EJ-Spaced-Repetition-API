9738047c8204e3d6e3c80627720ad9ec15d2051c
Daniel <herrnichols@gmail.com>
2019-11-21 12:54:08 -0500

    deployed server

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DB_URL || 'postgresql://daniel@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_SECRET: process.env.JWT_SECRET || 'deustchewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };

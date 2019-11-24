230370bef226537e6756a7444b6867154264534b
Daniel <herrnichols@gmail.com>
2019-11-24 17:59:40 -0500

    revert cors

--- src/app.js
+++ src/app.js
@@ -18,12 +18,6 @@ app.use(
 );
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://clevergerman.netlify.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 app.use(helmet());
 

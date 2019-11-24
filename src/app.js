cae1cb7239a1ff88f1345ccd16eae59140ebae4d
Daniel <herrnichols@gmail.com>
2019-11-24 17:45:32 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./middleware/error-handler')
-const authRouter = require('./auth/auth-router')
-const languageRouter = require('./language/language-router')
-const userRouter = require('./user/user-router')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth/auth-router');
+const languageRouter = require('./language/language-router');
+const userRouter = require('./user/user-router');
+const { CLIENT_ORIGIN } = require('./config');
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}))
-app.use(cors())
-app.use(helmet())
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test',
+  })
+);
 
-app.use('/api/auth', authRouter)
-app.use('/api/language', languageRouter)
-app.use('/api/user', userRouter)
+// app.use(cors({ origin: CLIENT_ORIGIN }));
 
-app.use(errorHandler)
+app.use(helmet());
 
-module.exports = app
+app.use('/api/auth', authRouter);
+app.use('/api/language', languageRouter);
+app.use('/api/user', userRouter);
+
+app.use(errorHandler);
+
+module.exports = app;
--- src/config.js
+++ src/config.js
@@ -5,4 +5,5 @@ module.exports = {
     process.env.DATABASE_URL || 'postgresql://daniel@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'deutschewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000"
 };

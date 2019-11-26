fccd5f170fe1c92cebac547356f2c7a35626e77e
Daniel <herrnichols@gmail.com>
2019-11-25 19:09:36 -0500

    added origin

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
-    process.env.DATABASE_URL || 'postgresql://daniel@localhost/spaced-repetition',
+    process.env.DATABASE_URL ||
+    'postgresql://daniel@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'deutschewoerter',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000"
+  CLIENT_ORIGIN: 'https://clevergerman.netlify.com',
 };
